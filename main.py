caba1481018e1dc58a4143c3cfd136b3e958b48b
Omar Hermamdez <omar.hertnandez@atomchat.io>
2022-06-10 00:42:27 -0500

    feat(example2): add sertvice callck http with or withou filters and download file results

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import codecs
+import json
+import os
+from typing import Union, Dict
+
 import http3 as http3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from starlette.responses import FileResponse
 
 from fastapi.openapi.docs import (
     get_redoc_html,
@@ -12,6 +19,7 @@ app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 client = http3.AsyncClient()
+
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
@@ -38,5 +46,37 @@ async def redoc_html():
     )
 
 
+class PayloadHttp(BaseModel):
+    url: str
+    filters: Union[Dict, None] = None
 
 
+async def callback_http(payload: PayloadHttp):
+    try:
+        if len(payload.filters) > 3:
+            raise HTTPException(status_code=400, detail="filters should not exceed more than 3 values!!")
+        r = await client.get(payload.url, params=payload.filters)
+        callback_serializable = r.json()
+        return callback_serializable
+    except BaseException as err:
+        return err
+
+
+@app.post("/fetch-http-resources/")
+async def fetch_http_resources(payload: PayloadHttp):
+    response = await callback_http(payload)
+    return response
+
+
+@app.post("/fetch-http-resources/download/")
+async def fetch_http_resources(payload: PayloadHttp):
+    try:
+        response = await callback_http(payload)
+        print(response)
+        file_name = "test.txt"
+        file_path = os.getcwd() + "/" + file_name
+        with open(file_name, 'wb+') as f:
+            json.dump(response, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+        return FileResponse(path=file_path, media_type='application/octet-stream', filename=file_name)
+    except HTTPException as err:
+        return err
